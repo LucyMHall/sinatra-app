925ab736f6dbd0fe6a3cb6529056b15609f85f51
LucyMHall <lucymhall1995@gmail.com>
2019-06-11 14:10:15 +0100

    add red border

--- app.rb
+++ app.rb
@@ -2,11 +2,10 @@ require 'sinatra'
 
 get '/cat' do
   "
-  <h1>
-    hello cat
-  </h1>
-  <div>
-    <img src='http://bit.ly/1eze8aE'>
+  <h1> Cat</h1>
+  <div style='border:2px dotted red'>
+  <h2> Meow </h2>
+  <img src='http://bit.ly/1eze8aE' >
   </div>
   "
 end
